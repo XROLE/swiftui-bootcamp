1448177df19ffbddc4df077cf6d4f4dfcbc3c6d2
xrole <xrolediamond@gmail.com>
2022-12-20 06:51:21 +0100

    first commit

---
+++ swiftui-bootcamp/swiftui_bootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftui_bootcampApp.swift
+//  swiftui-bootcamp
+//
+//  Created by Xrole Diamond on 20/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct swiftui_bootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
