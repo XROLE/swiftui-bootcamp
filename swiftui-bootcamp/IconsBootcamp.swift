1f191921a9d7d52fc51a8304fe66caf1b0d066d7
xrole <xrolediamond@gmail.com>
2022-12-21 17:15:08 +0100

    system images

---
+++ swiftui-bootcamp/IconsBootcamp.swift
@@ -0,0 +1,27 @@
+//
+//  IconsBootcamp.swift
+//  swiftui-bootcamp
+//
+//  Created by Xrole Diamond on 21/12/2022.
+//
+
+import SwiftUI
+
+struct IconsBootcamp: View {
+    var body: some View {
+    //    Image(systemName: "heart.fill")
+        //    .resizable()
+         //   .aspectRatio(contentMode: .fill)
+         //   .font(.title)
+         //   .foregroundColor(.purple)
+        //    .frame(width: 300, height: 300)
+        
+        Image("site")
+    }
+}
+
+struct IconsBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        IconsBootcamp()
+    }
+}
